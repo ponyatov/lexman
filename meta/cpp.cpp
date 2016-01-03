ee456684f00d1e04886351cdc9f3bc62bf0cd14d
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-03 18:08:02 +0400

    v meta/ dir works
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta/cpp.cpp
+++ meta/cpp.cpp
@@ -1,4 +1,59 @@
 #include "hpp.hpp"
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
-void W(sym*o) { cout<<o->dump(); }
+int main() { env_init(); return yyparse(); }
+
+void W(sym*o)		{ cout << o->dump(); }				// \ writers
+void W(string s)	{ cout << s; }						// /
+
+/////////////////// sym ////////////////////
+
+sym::sym(string T,string V)	{ tag=T; val=V; }
+void sym::push(sym*o)		{ nest.push_back(o); }
+sym* sym::eval()			{
+	if (env[val]) return env[val];
+	for (auto it=nest.begin();it!=nest.end();it++)
+		(*it) = (*it)->eval();
+	return this; }
+
+string sym::pad(int n)		{ string S; for (int i=0;i<n;i++) S+="\t"; return S; }
+string sym::tagval()		{ return "<"+tag+":"+val+">"; }
+string sym::dump(int depth)	{ string S = "\n"+pad(depth)+tagval();
+	for (auto it=nest.begin();it!=nest.end();it++)
+		S+=(*it)->dump(depth+1);
+	return S; }
+
+sym* sym::eq(sym*o)			{ env[val]=o; return o; }	// {B->A;ret B}
+sym* sym::at(sym*o)			{ push(o); return this; }
+
+//////////////////// scalars /////////////////////
+
+Sym::Sym(string V):sym("sym",V) {}
+Str::Str(string V):sym("str",V) {}
+string Str::tagval()			{ return "<"+tag+":'"+val+"'>"; }
+
+Int::Int(string V):sym("int",V) { i = atoi(val.c_str()); }
+
+//////////////////// functionals ////////////////
+
+Op::Op(string V):sym("op",V)		{}
+sym* Op::eval() {
+	sym::eval();
+	assert(nest.size()==2);
+	if (val=="@") return nest[0]->at(nest[1]);
+	return this;
+}
+
+Fn::Fn(string V,FN F):sym("fn",V)	{ fn=F; }
+sym* Fn::at(sym*o)					{ return fn(o); }
+
+///////////////////// fileio ////////////////////
+
+Dir::Dir(sym*o):sym("dir",o->val) { mkdir(o->val.c_str()); }
+
+sym* dir(sym*o)		{ return new Dir(o); }
+
+map<string,sym*> env;
+void env_init(){
+	env["dir"] = new Fn("dir",dir);
+}
--- meta/hpp.hpp
+++ meta/hpp.hpp
@@ -3,15 +3,51 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cassert>
+#include <vector>
+#include <map>
+#ifdef __MINGW32__
+	#include <direct.h>
+#else
+#endif
 using namespace std;
 
-struct sym {
-	string tag,val;
+struct sym {								////// dynamic symbol type
+	string tag,val;							// <tag:val> object repr
 	sym(string,string);
-	string dump();
+	virtual string dump(int depth=0);		// \ dumping 
+	virtual string tagval();
+	string pad(int);						// /
+	vector<sym*> nest;						// \ nested objects list
+	void push(sym*);						// /
+	virtual sym* eval();					// compute/evaluate object
+	virtual sym* eq(sym*);					// \\ operators
+	virtual sym* at(sym*);
 };
 
+extern map<string,sym*> env;				//////////// environment
+extern void env_init();
+											//////////// scalars
+struct Sym:sym { Sym(string); };			// symbol
+struct Str:sym { Str(string); 				// string
+	string tagval(); };
+
+struct Int:sym { Int(string); long i; };	// integer
+
+											//////////// functionals
+struct Op:sym { Op(string); sym*eval(); };	// operator
+typedef sym*(*FN)(sym*);					// fn ptr
+struct Fn:sym { Fn(string,FN); FN fn; 		// function
+	sym*at(sym*); };
+
+											//////////// fileio
+struct Dir:sym { Dir(sym*); };				// dir constructor
+extern sym* dir(sym*);
+struct File:sym { File(sym*); };			// file constructor
+extern sym* file(sym*);
+
 extern void W(sym*);
+extern void W(string);
 
 extern int yylex();
 extern int yylineno;
@@ -19,5 +55,6 @@ extern char* yytext;
 extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
 
 #endif // _H_bI
